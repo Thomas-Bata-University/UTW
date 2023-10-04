308ea0b3e0cabe8aa55ee1d7a54d5042a1dc8d3e
KamilChytil <75939657+KamilChytil@users.noreply.github.com>
2023-10-04 16:12:04 +0200

    Expand_database
    add lists

--- Assets/Scripts/Parts/Database.cs
+++ Assets/Scripts/Parts/Database.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
@@ -7,38 +8,68 @@ public class Database : MonoBehaviour
 {
     public List<GameObject> hulls;
     public List<GameObject> turrets;
+    public List<GameObject> canons;
+    public List<GameObject> suspensions;
+    public List<GameObject> maps;
     public UnityEvent isDbInitialized;
     
     public Preset SelectedPreset = null;
 
+    private string[] names = { "hull", "turret", "canon", "suspension", "map", };
+
+
+
+    private List<GameObject>[] arrayOfLists;
+
+
 
     void Start()
     {
+        InitializeArrayOfLists();
         //hulls = Resources.LoadAll<GameObject>("Hulls");
         //turrets = Resources.LoadAll<GameObject>("Turrets");
         Initialize();
     }
-     void Initialize()
+    void Initialize()
     {
-        string[] files = System.IO.Directory.GetFiles(Application.streamingAssetsPath, "*.");
+        string[] files = Directory.GetFiles(Application.streamingAssetsPath, "*.");
         foreach (string file in files)
         {
-            var loadedBundle
-            = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, file));
+            var loadedBundle = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, file));
 
             if (loadedBundle == null)
             {
                 Debug.Log("Failed to load AssetBundle!");
                 return;
             }
-
             var assets = loadedBundle.LoadAllAssets<GameObject>();
+
             foreach (var asset in assets)
             {
-                if (asset.name.EndsWith("MainBody")) hulls.Add(asset);
-                if (asset.name.EndsWith("Turret")) turrets.Add(asset);
+                for (int i = 0; i < arrayOfLists.Length; i++)
+                {
+                        if (asset.name.EndsWith(names[i], StringComparison.OrdinalIgnoreCase))
+                        {
+                            arrayOfLists[i].Add(asset);
+                        }
+                }
             }
+
+            
+            isDbInitialized.Invoke();
         }
-        isDbInitialized.Invoke();
     }
+
+    void InitializeArrayOfLists()
+    {
+        arrayOfLists = new List<GameObject>[]
+        {
+            hulls,
+            turrets,
+            canons,
+            suspensions,
+            maps
+        };
+    }
+
 }
