993a76fb2ebf1a84d9568fa108d25eadad967d2c
Tomas Pavlik <105510734+YiroDev@users.noreply.github.com>
2023-11-12 14:15:54 +0100

    Yiro dev/scene management (#42)
    * Logic for managing scenes
    
    * Load GAME scene for connected clients to lobby scene instance

---
+++ Assets/Scripts/Managers/SceneManager.cs
@@ -0,0 +1,102 @@
+using FishNet;
+using FishNet.Connection;
+using FishNet.Managing.Scened;
+using FishNet.Object;
+using FishNet.Transporting;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SceneManager : NetworkBehaviour {
+
+    public static SceneManager Instance;
+
+    private List<SceneData> sceneDataList = new List<SceneData>();
+
+    private void Start() {
+        if (Instance == null)
+            Instance = this;
+        InstanceFinder.NetworkManager.ServerManager.OnRemoteConnectionState += MoveClientToShardScene;
+    }
+
+    #region Lobby scene
+    [ServerRpc(RequireOwnership = false)]
+    public void CreateLobby(NetworkConnection conn) {
+        Debug.Log($"Creating new lobby by client ID: {conn.ClientId}...");
+        LoadScene(conn, new SceneLookupData(GameSceneUtils.LOBBY_SCENE), true);
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    public void ConnectToLobby(NetworkConnection conn, int handle) {
+        Debug.Log($"Connecting client ID: {conn.ClientId} to lobby...");
+        LoadScene(conn, new SceneLookupData(handle), false);
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    public void DisconnectLobby(NetworkConnection conn) {
+        Debug.Log($"Disconnecting client ID: {conn.ClientId} from lobby...");
+        LoadScene(conn, new SceneLookupData(GameSceneUtils.SHARD_SCENE), false);
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    public void RefreshLobby(NetworkConnection conn) {
+        sceneDataList.Clear();
+        foreach (var pair in InstanceFinder.SceneManager.SceneConnections) {//TODO-YIRO change to CRUD and use SyncList
+            if (pair.Key.name == GameSceneUtils.LOBBY_SCENE) {
+                SceneData data = new SceneData(pair.Key.handle, pair.Key.name, null, pair.Value.Count); //TODO-YIRO add LOBBY NAME
+                sceneDataList.Add(data);
+            }
+        }
+        RefreshLobbyResponse(conn, sceneDataList);
+    }
+
+    [TargetRpc]
+    public void RefreshLobbyResponse(NetworkConnection conn, List<SceneData> sceneDataList) {
+        FindObjectOfType<ShardController>().CreateLobbyButtons(sceneDataList); //TODO-YIRO use Find by tag
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    public void StartGame(NetworkConnection conn) { //TODO-YIRO remove lobby from sceneDataList
+        LoadScene(InstanceFinder.SceneManager.SceneConnections.First(pair => pair.Value.Contains(conn)).Value.ToArray(),
+            new SceneLookupData(GameSceneUtils.GAME_SCENE),
+            false);
+    }
+    #endregion
+
+    #region Shard scene
+    //Called on SERVER
+    private void MoveClientToShardScene(NetworkConnection conn, RemoteConnectionStateArgs args) {
+        if (args.ConnectionState != RemoteConnectionState.Started) return;
+
+        SceneLoadData data = new SceneLoadData(GameSceneUtils.SHARD_SCENE);
+        data.ReplaceScenes = ReplaceOption.All;
+
+        InstanceFinder.SceneManager.LoadConnectionScenes(conn, data);
+    }
+    #endregion
+
+    /// <summary>
+    /// Load scene.
+    /// </summary>
+    /// <param name="conn">Load scene for one Network connection</param>
+    /// <param name="lookupData">Data to find a scene</param>
+    /// <param name="allowStacking">True - create new scene instance | False - find existing scene</param>
+    private void LoadScene(NetworkConnection conn, SceneLookupData lookupData, bool allowStacking) {
+        SceneLoadData sceneLoadData = new SceneLoadData(lookupData);
+        sceneLoadData.Options.AllowStacking = allowStacking;
+        sceneLoadData.ReplaceScenes = ReplaceOption.OnlineOnly;
+        InstanceFinder.SceneManager.LoadConnectionScenes(conn, sceneLoadData);
+    }
+
+    private void LoadScene(NetworkConnection[] conns, SceneLookupData lookupData, bool allowStacking) {
+        SceneLoadData sceneLoadData = new SceneLoadData(lookupData);
+        sceneLoadData.Options.AllowStacking = allowStacking;
+        sceneLoadData.ReplaceScenes = ReplaceOption.OnlineOnly;
+        InstanceFinder.SceneManager.LoadConnectionScenes(conns, sceneLoadData);
+    }
+
+    private void OnDestroy() {
+        InstanceFinder.NetworkManager.ServerManager.OnRemoteConnectionState -= MoveClientToShardScene;
+    }
+
+}
--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,6 +1,3 @@
-using FishNet;
-using FishNet.Object;
-using FishNet.Transporting;
 using FishNet.Transporting.Tugboat;
 using UnityEngine;
 
@@ -16,24 +13,10 @@ public class Menu : MonoBehaviour {
         } else {
             Debug.LogError("Couldn't find Tugboat component!");
         }
-        InstanceFinder.NetworkManager.ServerManager.OnServerConnectionState += SpawnPresetManagerOnServer;
     }
 
     public void Host() {
         _tugboat.StartConnection(true);
-    }
-
-    /// <summary>
-    /// If STATE is STARTED (server started) then spawn PRESET MANAGER only on SERVER.
-    /// </summary>
-    /// <param name="args"></param>
-    private void SpawnPresetManagerOnServer(ServerConnectionStateArgs args) {
-        if (args.ConnectionState == LocalConnectionState.Started) {
-            var presetManagerInstance = Instantiate(presetManager);
-            presetManagerInstance.GetComponent<NetworkObject>().SetIsGlobal(true);
-            presetManagerInstance.GetComponent<NetworkObject>().SetIsNetworked(true);
-            InstanceFinder.ServerManager.Spawn(presetManagerInstance);
-        }
     }
 
     public void ExitGame() {
