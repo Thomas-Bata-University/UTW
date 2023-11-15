603d6edb43b8ea2bc097f29f34734d8383e8c4ea
timok19 <tima1199@gmail.com>
2023-11-15 17:45:02 +0100

    Update HullDropdown.cs

--- Assets/Scripts/Garage/HullDropdown.cs
+++ Assets/Scripts/Garage/HullDropdown.cs
@@ -6,25 +6,23 @@ public class HullDropdown : MonoBehaviour
     private GameObject _assetDb;
     private Database _dbComponent;
     private Dropdown _dropdown;
-    
+
     public void Start()
     {
+        _dropdown = transform.GetComponent<Dropdown>();
+        _dropdown.options.Clear();
+
+        _assetDb = GameObject.Find("AssetDatabase");
+        _dbComponent = (Database)_assetDb.GetComponent(typeof(Database));
+
+        Debug.Log(_dbComponent.hulls.Count == 0 ? "No hulls!" : "There are hulls!");
+
+        foreach (var hull in _dbComponent.hulls)
         {
-            _dropdown = transform.GetComponent<Dropdown>();
-            _dropdown.options.Clear();
-
-            _assetDb = GameObject.Find("AssetDatabase");
-            _dbComponent = (Database)_assetDb.GetComponent(typeof(Database));
-
-            Debug.Log(_dbComponent.hulls.Count == 0 ? "No hulls!" : "There are hulls!");
-
-            foreach (var hull in _dbComponent.hulls)
+            _dropdown.options.Add(new Dropdown.OptionData
             {
-                _dropdown.options.Add(new Dropdown.OptionData
-                {
-                    text = hull.name
-                });
-            }
+                text = hull.name
+            });
         }
     }
 }
